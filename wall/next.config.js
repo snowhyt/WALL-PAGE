d0f561d2a82940bf45ebeb92ae1760cda7716a3c
unknown <oliverglorioso.44@gmail.com>
2025-07-05 22:59:38 +0800

    add disable eslint when build

--- wall/next.config.js
+++ wall/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['lquxnjdzuzsrnuoybgvk.supabase.co'],
   },
+  eslint: {
+    // ✅ This disables ESLint ONLY when running `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
